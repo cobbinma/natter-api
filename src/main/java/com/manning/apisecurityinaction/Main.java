805160825a4bd3dbdc8f540fcb585d15f60ea6e0
matthew.cobbing <cobbinma@gmail.com>
2021-09-13 11:14:26 +0100

    development

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -2,10 +2,12 @@ package com.manning.apisecurityinaction;
 
 import java.nio.file.*;
 
-import com.manning.apisecurityinaction.controller.SpaceController;
-import org.dalesbred.*;
-import org.h2.jdbcx.*;
+import com.manning.apisecurityinaction.controller.*;
+import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
+import org.h2.jdbcx.JdbcConnectionPool;
 import org.json.*;
+import spark.*;
 
 import static spark.Spark.*;
 
@@ -20,16 +22,44 @@ public class Main {
         database = Database.forDataSource(datasource);
 
         var spaceController = new SpaceController(database);
-        post("/spaces", spaceController::createSpace);
 
-        after((request, response) -> {
-            response.type("application/json");
+        before(((request, response) -> {
+            if (request.requestMethod().equals("POST") &&
+                    !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put(
+                        "error", "Only application/json supported"
+                ).toString());
+            }
+        }));
+
+        afterAfter((request, response) -> {
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
         });
+
+        post("/spaces", spaceController::createSpace);
 
         internalServerError(new JSONObject()
                 .put("error", "internal server error").toString());
         notFound(new JSONObject()
                 .put("error", "not found").toString());
+
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+                (e, request, response) -> response.status(404));
+    }
+
+    private static void badRequest(Exception ex,
+                                   Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject().put("error", ex.getMessage()).toString());
     }
 
     public static void createTables(Database database) throws Exception {
