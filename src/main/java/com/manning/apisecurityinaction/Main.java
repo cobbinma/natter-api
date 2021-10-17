45c37d557b2286fd368cfe0fb92f6e28a4beffb7
matthew.cobbing <cobbinma@gmail.com>
2021-10-17 15:52:58 +0100

    rate limiter

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -2,6 +2,7 @@ package com.manning.apisecurityinaction;
 
 import java.nio.file.*;
 
+import com.google.common.util.concurrent.*;
 import com.manning.apisecurityinaction.controller.*;
 import org.dalesbred.Database;
 import org.dalesbred.result.EmptyResultException;
@@ -23,13 +24,22 @@ public class Main {
 
         var spaceController = new SpaceController(database);
 
-        before(((request, response) -> {
-            if (request.requestMethod().equals("POST") &&
-                    !"application/json".equals(request.contentType())) {
-                halt(415, new JSONObject().put(
-                        "error", "Only application/json supported"
-                ).toString());
+        var rateLimiter = RateLimiter.create(2.0d);
+
+        before((request, response) -> {
+            if (!rateLimiter.tryAcquire()) {
+                response.header("Retry-After", "2");
+                halt(429);
             }
+        });
+
+        before(((request, response) -> {
+                if (request.requestMethod().equals("POST") &&
+                        !"application/json".equals(request.contentType())) {
+                    halt(415, new JSONObject().put(
+                            "error", "Only application/json supported"
+                    ).toString());
+                }
         }));
 
         afterAfter((request, response) -> {
