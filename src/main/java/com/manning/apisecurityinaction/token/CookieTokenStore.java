ffa2c15c20e783bd58b1430261352d0f81ad93ff
matthew.cobbing <cobbinma@gmail.com>
2021-11-16 18:00:43 +0000

    avoid session fixation

--- src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
@@ -8,7 +8,11 @@ public class CookieTokenStore implements TokenStore {
 
     @Override
     public String create(Request request, Token token) {
-        var session = request.session(true);
+        var session = request.session(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        session = request.session(true);
 
         session.attribute("username", token.username);
         session.attribute("expiry", token.expiry);
