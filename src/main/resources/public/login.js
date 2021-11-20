675d9d0d1268092930687cf9b95c9fe702c875fa
matthew.cobbing <cobbinma@gmail.com>
2021-11-20 15:06:50 +0000

    login

---
+++ src/main/resources/public/login.js
@@ -0,0 +1,34 @@
+const apiUrl = "https://localhost:4567";
+
+const login = (username, password) => {
+    let credentials = 'Basic ' + btoa(username + ':' + password);
+
+    fetch(apiUrl + '/sessions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': credentials
+        },
+    }).then(res => {
+        if (res.ok) {
+            res.json().then(json => {
+                document.cookie = 'csrfToken' + json.token + ';Secure;SameSite=strict';
+                window.location.replace('/natter.html')
+            })
+        }
+    }).catch(error => console.error('Error logging in: ', error))
+}
+
+window.addEventListener('load', e => {
+    document.getElementById('login').addEventListener('submit', processLoginSubmit)
+})
+
+const processLoginSubmit = e => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    login(username, password);
+    return false;
+}
--- src/main/resources/public/natter.js
+++ src/main/resources/public/natter.js
@@ -2,22 +2,26 @@ const apiUrl = 'https://localhost:4567'
 
 const createSpace = (name, owner) => {
     let data = {name: name, owner: owner};
+    let csrfToken = getCookie('csrfToken');
 
     fetch(apiUrl + '/spaces', {
         method: 'POST',
         credentials: 'include',
         body: JSON.stringify(data),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrfToken
         }
     }).then(response => {
         if (response.ok) {
             return response.json()
+        } else if (response.status === 401) {
+            window.location.replace('/login.html')
         } else {
             throw Error(response.statusText)
         }
     }).then(json => console.log('created space: ', json.name, json.uri))
-    .catch(error => console.error('error: ', error));
+        .catch(error => console.error('error: ', error));
 }
 
 window.addEventListener('load', function (e) {
@@ -33,4 +37,12 @@ const processFormSubmit = (e) => {
     createSpace(spaceName, owner);
 
     return false;
+}
+
+const getCookie = (cookieName) => {
+    const cookieValue = document.cookie.split(';')
+        .map(item => item.split('=').map(x => decodeURIComponent(x.trim())))
+        .filter(item => item[0] === cookieName)[0]
+
+    if (cookieValue) return cookieValue[1];
 }
